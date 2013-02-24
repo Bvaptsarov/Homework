dad580615b85b81eb6f8e3601331e2f066b80301
Borislav Vaptsarov <b.vaptsarov@gmail.com>
2013-02-24 14:32:30 +0200

    Updating with 5 new solutions

---
+++ 10.OOPSecondPart/GenericList/TestGenericList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    class TestGenericList
+    {
+        static void Main()
+        {
+            GenericList<int> list = new GenericList<int>();
+            list.AddElement(10);
+            list.AddElement(5);
+            list.AddElement(4);
+            list.InsertElement(1, 15);
+            list.InsertElement(1, 12);
+            Console.WriteLine(list.ToString());
+            Console.WriteLine();
+            list[0] = 5;
+            int min = list.Min();
+            int max = list.Max();
+            Console.WriteLine(min);
+            Console.WriteLine(max);
+            list.Clear();
+            Console.WriteLine(list.ToString());
+        }
+    }
+}
