525f6f08d0a4e4f0468716558546f0bcef62883a
Borislav Vaptsarov <b.vaptsarov@gmail.com>
2013-03-02 20:30:58 +0200

    Hot Fix

--- 11.Extension Methods and LINQ/TimerDelegate/CallBack.cs
+++ 11.Extension Methods and LINQ/TimerDelegate/CallBack.cs
@@ -4,7 +4,7 @@ using System.Threading;
 
 namespace TimerDelegate
 {
-    public delegate void TimeDelegate(Object state);
+    public delegate void CallBack(object state);
 
     public class TimerCallBack
     {
@@ -15,9 +15,8 @@ namespace TimerDelegate
 
         public static void Main()
         {
-            TimeDelegate timerDelegate = new TimeDelegate(TimeNow);
-            TimerCallback callback = new TimerCallback(timerDelegate);
-            Timer time = new Timer(callback, null, 0, 1000);
+            CallBack callback = new CallBack(TimeNow);
+            Timer time = new Timer(callback.Invoke, null, 0, 1000);
 
             Console.Read(); //press enter to exit
         }
