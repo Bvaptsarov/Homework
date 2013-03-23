8a8dc43d2704bbc19350a2e8e6d3bfb6f484d2b2
Borislav Vaptsarov <b.vaptsarov@gmail.com>
2013-03-23 16:25:34 +0200

    TicTacToeTeamCasper

---
+++ TicTacToeTeamCasper/PlayerInitialize/HumanPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TicTacToe.PlayerInitialize
+{
+    public class HumanPlayer : Player, IDrawable
+    {
+        public Boolean IsMyTurn { get; set; }
+
+        public HumanPlayer(String name, short id, TypeOfSign sign)
+            : base(name, id, sign)
+        { }
+
+        public void Draw()
+        {
+            // TODO: Implement this method
+            throw new NotImplementedException();
+        }
+    }
+}
