9c385f674892e17ab05912f47153ff870abf0d0a
Borislav Vaptsarov <b.vaptsarov@gmail.com>
2013-05-02 18:26:30 +0300

    Documention added

---
+++ 17.Documentation/StringExtensionsTest/TestExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using StringExtensions;
+
+namespace StringExtensionsTest
+{
+    class TestExtensions
+    {
+        static void Main(string[] args)
+        {
+            string testingString = "I am a student, therefore i'm poor!";
+
+            Console.WriteLine(testingString.ToMd5Hash());
+            Console.WriteLine("".ToBoolean());
+
+            Console.WriteLine(string.Empty.ToShort());
+            Console.WriteLine(testingString.ToDateTime());
+            Console.WriteLine("hello world.".CapitalizeFirstLetter());
+
+            Console.WriteLine("Здравей свят.".ConvertCyrillicToLatinLetters());
+            Console.WriteLine("Zdravey svqt.".ConvertLatinToCyrillicKeyboard());
+
+            Console.WriteLine("borisla21321(*67123)".ToValidUsername());
+            Console.WriteLine("NotValid file name ***lalala*** .docx".ToValidLatinFileName());
+            Console.WriteLine("Test".GetFirstCharacters(3));
+            Console.WriteLine("Correct-File-Name.txt.exe".GetFileExtension());
+            Console.WriteLine("hello beautiful kind world.".GetStringBetween("hello", "world"));
+            Console.WriteLine("doc".ToContentType());
+            
+            var arr = "Hello world.".ToByteArray();
+            Console.WriteLine(String.Join(" ",arr));
+        }
+    }
+}
